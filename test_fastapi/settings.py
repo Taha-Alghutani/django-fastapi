b752ca44a86fb982fc7b24de368b969cb2463a84
Taha-BG <alghutanitaha@gmail.com>
2025-07-03 15:28:56 +0300

    dkfjdk

--- test_fastapi/settings.py
+++ test_fastapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j&o!g+*5vjyd)3buxngjamja8bt_rar9n_j!75gn62q67ygqhu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','54.178.147.74']
 
